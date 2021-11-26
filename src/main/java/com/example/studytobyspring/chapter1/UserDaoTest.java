f833a033cd4b5a1c57e1dff05cc7909206c9c1a1
jungho <jungpark007@naver.com>
2021-11-27 07:52:03 +0900

    DaoFactory @Bean 등록

--- src/main/java/com/example/studytobyspring/chapter1/UserDaoTest.java
+++ src/main/java/com/example/studytobyspring/chapter1/UserDaoTest.java
@@ -4,6 +4,8 @@ import com.example.studytobyspring.chapter1.user.dao.DConnectionMaker;
 import com.example.studytobyspring.chapter1.user.dao.DaoFactory;
 import com.example.studytobyspring.chapter1.user.dao.UserDao;
 import com.example.studytobyspring.chapter1.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -11,9 +13,11 @@ public class UserDaoTest {
     // 테스트의 책임,
     // 오브젝트 간의 관계를 맺는 책임이 생겨버림. => factory 오브젝트 생성
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new DaoFactory().userDao();
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        System.out.println("userDao = " + userDao);
 
         // 테스트 코드...
-
     }
 }
--- src/main/java/com/example/studytobyspring/chapter1/user/dao/DaoFactory.java
+++ src/main/java/com/example/studytobyspring/chapter1/user/dao/DaoFactory.java
@@ -1,6 +1,12 @@
 package com.example.studytobyspring.chapter1.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+
+    @Bean
     // UserDao 의 생성 책임을 맡은 팩토리 메소드 
     public UserDao userDao(){
         return new UserDao(connectionMaker());
@@ -14,7 +20,8 @@ public class DaoFactory {
         return new MessageDao(connectionMaker());
     }
 
-    private DConnectionMaker connectionMaker() {
+    @Bean
+    public DConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
 }
