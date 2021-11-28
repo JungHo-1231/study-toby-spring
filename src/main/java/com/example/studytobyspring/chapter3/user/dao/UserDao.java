96c04c9aa3a55aa402a41f0bd3c82eede933f880
jungho <jungpark007@naver.com>
2021-11-28 10:47:52 +0900

    chapter 3 UserDAo - AddStatement 내부 클래스 생성

--- src/main/java/com/example/studytobyspring/chapter3/user/dao/UserDao.java
+++ src/main/java/com/example/studytobyspring/chapter3/user/dao/UserDao.java
@@ -17,8 +17,21 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        AddStatement stm = new AddStatement(user);
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        // 내부 클래스로 이동
+        class AddStatement implements StatementStrategy{
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name , password) values (?,?,?)");
+
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        }
+        AddStatement stm = new AddStatement();
         jdbcContextWithStatementStrategy(stm);
     }
 
